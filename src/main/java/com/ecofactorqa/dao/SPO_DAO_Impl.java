ee41d9a8ff05e9a951cf8a050b5b61f70dfd6094
vlobanov-ecofactor <vitaliy.lobanov@ecofactor.com>
2015-07-10 15:46:41 -0700

    add 191 spo

--- src/main/java/com/ecofactorqa/dao/SPO_DAO_Impl.java
+++ src/main/java/com/ecofactorqa/dao/SPO_DAO_Impl.java
@@ -25,6 +25,10 @@ public class SPO_DAO_Impl {
     public static int end_spo_thermostat_id_thermostat_event=0;
     public static int end_spo_thermostat_id_thermostat_algoritm=0;
     public static int end_spo_thermostat_id_mo=0;
+    public static int start_spo_thermostat_id_thermostat_event_heat=0;
+    public static int start_spo_thermostat_id_thermostat_algoritm_heat=0;
+    public static int end_spo_thermostat_id_thermostat_event_heat=0;
+    public static int end_spo_thermostat_id_thermostat_algoritm_heat=0;
     
 
 	public static void insertStartSpoEntry(int t_id, int event_ee_start, String next_phase_time_start, String date_setup_start, String execution_start_time_utc_start, String execution_end_time_utc_start, String mo_cutoff_time_utc_start) {
@@ -165,4 +169,123 @@ public class SPO_DAO_Impl {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void insertStartSpoEntryHeat(int t_id, int event_ee_start, String next_phase_time_start, String date_setup_start, String execution_start_time_utc_start, String execution_end_time_utc_start, String mo_cutoff_time_utc_start) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(ef_11_db_url,ef_11_db_user,ef_11_db_pass);
+		    Statement statment = connection.createStatement();
+
+		    String sql = "INSERT INTO ef_thermostat_algo_control (thermostat_id, algorithm_id, thermostat_algorithm_status, algorithm_phase, "
+		    		+ "setting_phase_0, setting_prev_phase, next_phase_time, date_setup, execution_start_time_utc, execution_end_time_utc, mo_cutoff_time_utc, mo_recovery, mo_blackout, actor) "
+		    		+ "VALUES ('" + t_id + "', 191, 'ACTIVE', 0, '" + event_ee_start + "', 0, "
+		    		+ "'" + next_phase_time_start + "', '" + date_setup_start + "', '" + execution_start_time_utc_start + "', '" + execution_end_time_utc_start + "', '" + mo_cutoff_time_utc_start + "', "
+		    		+ "'2.0', 2, 'SPO')"; 
+		    statment.executeUpdate(sql);
+		    System.out.println(sql);
+	        
+		} 
+		catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void insertEndSpoEntryHeat(int t_id, String next_phase_time_end, String date_setup_end, String execution_start_time_utc_end, String mo_cutoff_time_utc_end) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(ef_11_db_url,ef_11_db_user,ef_11_db_pass);
+		    Statement statment = connection.createStatement();
+
+		    String sql = "INSERT INTO ef_thermostat_algo_control (thermostat_id, algorithm_id, thermostat_algorithm_status, algorithm_phase, "
+		    		+ "setting_phase_0, setting_prev_phase, next_phase_time, date_setup, execution_start_time_utc, mo_cutoff_time_utc, mo_recovery, mo_blackout, actor) "
+		    		+ "VALUES ('" + t_id + "', 191, 'ACTIVE', 0, 0, 0, "
+		    		+ "'" + next_phase_time_end + "', '" + date_setup_end + "', '" + execution_start_time_utc_end + "', '" + mo_cutoff_time_utc_end + "', '2.0', 2, 'SPO')"; 
+		    statment.executeUpdate(sql);
+		    System.out.println(sql);
+	        
+		} 
+		catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void start_SPO_efts_heat(int t_id, int event_ee_start) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(efts_db_url,efts_db_user,efts_db_pass);
+		    Statement statment = connection.createStatement();
+
+	        String sql = "SELECT * FROM ef_thermostat_event where thermostat_id= '" + t_id + "' and algorithm_id=191 and event_ee= '" + event_ee_start + "' and event_status='PROCESSED' and event_sys_time between timestamp (DATE_sub(now(), interval 600 SECOND)) and timestamp(now()) order by event_sys_time DESC limit 1;";	        
+	        ResultSet result = statment.executeQuery(sql);
+	        while (result.next()) {
+	        	start_spo_thermostat_id_thermostat_event_heat = result.getInt("thermostat_id");
+	        }      	        
+		} 
+		catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void start_SPO_ef11_heat(int t_id, int setting_phase_0_start) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(ef_11_db_url,ef_11_db_user,ef_11_db_pass);
+		    Statement statment = connection.createStatement();
+
+	        String sql = "SELECT * FROM ef_thermostat_algo_control where thermostat_id= '" + t_id + "' and algorithm_id=191 and setting_phase_0= '" + setting_phase_0_start + "' and thermostat_algorithm_status='PROCESSED' and next_phase_time between timestamp (DATE_sub(now(), interval 600 SECOND)) and timestamp(now()) order by next_phase_time DESC limit 1;";	        
+	        ResultSet result = statment.executeQuery(sql);
+	        while (result.next()) {
+	        	start_spo_thermostat_id_thermostat_algoritm_heat = result.getInt("thermostat_id");
+	        }      	        
+		} 
+		catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void end_SPO_efts_heat(int t_id, int event_ee_end) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(efts_db_url,efts_db_user,efts_db_pass);
+		    Statement statment = connection.createStatement();
+
+	        String sql = "SELECT * FROM ef_thermostat_event where thermostat_id= '" + t_id + "' and algorithm_id=191 and event_ee= '" + event_ee_end + "' and event_status='PROCESSED' and event_sys_time between timestamp (DATE_sub(now(), interval 720 SECOND)) and timestamp(now()) order by event_sys_time DESC limit 1;";	        
+	        ResultSet result = statment.executeQuery(sql);
+	        while (result.next()) {
+	        	end_spo_thermostat_id_thermostat_event_heat = result.getInt("thermostat_id");
+	        }      	        
+		} 
+		catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void end_SPO_ef11_heat(int t_id, int setting_phase_0_end) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(ef_11_db_url,ef_11_db_user,ef_11_db_pass);
+		    Statement statment = connection.createStatement();
+
+	        String sql = "SELECT * FROM ef_thermostat_algo_control where thermostat_id= '" + t_id + "' and algorithm_id=191 and setting_phase_0= '" + setting_phase_0_end + "' and thermostat_algorithm_status='PROCESSED' and next_phase_time between timestamp (DATE_sub(now(), interval 720 SECOND)) and timestamp(now()) order by next_phase_time DESC limit 1;";	        
+	        ResultSet result = statment.executeQuery(sql);
+	        while (result.next()) {
+	        	end_spo_thermostat_id_thermostat_algoritm_heat = result.getInt("thermostat_id");
+	        }      	        
+		} 
+		catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
 }
--- src/test/java/com/ecofactor/spo/SPO.java
+++ src/test/java/com/ecofactor/spo/SPO.java
@@ -201,6 +201,126 @@ public class SPO {
 		System.out.println("SPO was correctly ended. SPO verification is done.");
 	}
 	
+	@Test //PLAT -
+	public void createSpoForHeatThermostat_191(){
+		//set t_stat to heat mode
+		String jsonString = APIprop.json_state_valid_hvac_mode_heat;
+		Invocation.Builder invocationBuilderHVACMode = client.target(thermostatStateURL).request(MediaType.APPLICATION_JSON);
+		Response responseCoolHvacMode = invocationBuilderHVACMode.put(Entity.json(jsonString));
+		WaitUtil.tinyWait();	
+		Assert.assertTrue(responseCoolHvacMode.getStatus() == 200,"Expected status 200. Actual status is :"+ responseCoolHvacMode.getStatus());
+		System.out.println(responseCoolHvacMode);
+		
+		WaitUtil.tinyWait();
+		Invocation.Builder invocationBuilder = client.target(thermostatStateURL).request(MediaType.APPLICATION_JSON);
+		Response response = invocationBuilder.get();
+		String content = response.readEntity(String.class);
+		Assert.assertTrue(response.getStatus() == 200,"Expected status 200. Actual status is :"+ response.getStatus());
+		Assert.assertTrue(content.contains("\"hvac_mode\":\"heat\""),"Expected hvac_mode Heat");
+		Assert.assertTrue(content.contains("\"setpoint_reason\":\"schedule\""),"Expected set_point_reason should be schedule");
+
+		//PArsing json response
+        final JSONObject jsonObject = JsonUtil.parseObject(content);
+        final JSONObject knownstate = (JSONObject) jsonObject.get("best_known_current_state_thermostat_data");
+        
+        //Printing out State of The t_stat
+        final String setpoint_reason = jsonObject.get("setpoint_reason").toString();
+        System.out.println("setpoint_reason--- " + setpoint_reason);
+        
+        final String hvac_mode = knownstate.get("hvac_mode").toString();
+        System.out.println("hvac_mode--- " + hvac_mode);
+        
+        final String heat_setpoint = knownstate.get("heat_setpoint").toString();
+        System.out.println("heat_setpoint--- " + heat_setpoint);
+		
+		WaitUtil.tinyWait();
+		WaitUtil.tinyWait();
+		
+		//set time for next_phase_time, execution_start_time_utc.
+        SimpleDateFormat dateFormatUTC = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        dateFormatUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date()); // Now use today date.
+        calendar.add(Calendar.MINUTE, 3); // Adding 3 min
+        next_phase_time_start = dateFormatUTC.format(calendar.getTime());
+        execution_start_time_utc_start = next_phase_time_start;
+        
+		//set time for execution_end_time_utc.
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.setTime(new Date()); // Now use today date.
+        calendar1.add(Calendar.MINUTE, 10); // Adding 10 min
+        execution_end_time_utc_start = dateFormatUTC.format(calendar1.getTime());
+        
+		//set time for date_setup,mo_cutoff_time_utc.
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.setTime(new Date()); // Now use today date.
+        calendar2.add(Calendar.MINUTE, 1); // Adding 1 min
+        date_setup_start = dateFormatUTC.format(calendar2.getTime());
+        mo_cutoff_time_utc_start = date_setup_start;
+ 		
+        System.out.println("Inserting SPO start entry into ef 11 DB......");
+        //insert start spo entry
+		SPO_DAO_Impl.insertStartSpoEntryHeat(t_id, event_ee_start, next_phase_time_start, date_setup_start, execution_start_time_utc_start, execution_end_time_utc_start, mo_cutoff_time_utc_start);
+		
+		//time conversion for end SPO entry
+		next_phase_time_end = execution_end_time_utc_start;
+		date_setup_end = date_setup_start;
+		execution_start_time_utc_end = execution_end_time_utc_start;
+		mo_cutoff_time_utc_end = mo_cutoff_time_utc_start;
+		
+		System.out.println("Inserting SPO end entry into ef 11 DB......");
+		//insert end spo entry
+		SPO_DAO_Impl.insertEndSpoEntryHeat(t_id, next_phase_time_end, date_setup_end, execution_start_time_utc_end, mo_cutoff_time_utc_end);		
+		System.out.println("SPO entries were created in db. Waiting to verify SPO starts correctly.......... ");
+		
+		//verify start setpoint was applied to the t_stat using state api call
+		WaitUtil.hugeWait();
+
+		Invocation.Builder invocationBuilder1 = client.target(thermostatStateURL).request(MediaType.APPLICATION_JSON);
+		Response response1 = invocationBuilder1.get();
+		String content1 = response1.readEntity(String.class);
+		Assert.assertTrue(response1.getStatus() == 200,"Expected status 200. Actual status is :"+ response1.getStatus());
+		Assert.assertTrue(content1.contains("\"hvac_mode\":\"heat\""),"Expected hvac_mode HEAT");
+		Assert.assertTrue(content1.contains("\"setpoint_reason\":\"ee\""),"Expected set_point_reason EE");
+		//Assert.assertTrue(content1.contains("\"cool_setpoint\":73"),"Expected cool_setpoint 73"); // 73F = 71F (is latest mo line 95) + 2F (line 31) 
+		System.out.println("Current Thermostat state is " + content1);
+		
+		//efts sart db verification
+		WaitUtil.tinyWait();
+		SPO_DAO_Impl.start_SPO_efts_heat(t_id, event_ee_start);
+		Assert.assertEquals(SPO_DAO_Impl.start_spo_thermostat_id_thermostat_event_heat, t_id);
+		
+		//ef11 start db verification
+		WaitUtil.tinyWait();
+		SPO_DAO_Impl.start_SPO_ef11_heat(t_id, setting_phase_0_start);
+		Assert.assertEquals(SPO_DAO_Impl.start_spo_thermostat_id_thermostat_algoritm_heat, t_id);
+		System.out.println("SPO was correctly started. Waiting to verify SPO ends correctly........");
+		
+		//verify end setpoint was applied to the t_stat using state api call
+		WaitUtil.hugeWait();
+		WaitUtil.threeMinutesWait();
+		Invocation.Builder invocationBuilder2 = client.target(thermostatStateURL).request(MediaType.APPLICATION_JSON);
+		Response response2 = invocationBuilder2.get();
+		String content2 = response2.readEntity(String.class);
+		Assert.assertTrue(response2.getStatus() == 200,"Expected status 200. Actual status is :"+ response2.getStatus());
+		Assert.assertTrue(content2.contains("\"hvac_mode\":\"heat\""),"Expected hvac_mode Heat");
+		Assert.assertFalse(content2.contains("\"setpoint_reason\":\"ee\""),"Expected set_point_reason EE");
+//		Assert.assertTrue(content2.contains("\"setpoint_reason\":\"schedule\""),"Expected set_point_reason schedule");
+//		Assert.assertTrue(content2.contains("\"cool_setpoint\":71"),"Expected cool_setpoint 71"); // 71F = 71F (is latest mo line 95)
+		System.out.println("Current Thermostat state is " + content2);
+				
+		//efts end db verification
+		WaitUtil.tinyWait();
+		SPO_DAO_Impl.end_SPO_efts_heat(t_id, event_ee_end);
+		Assert.assertEquals(SPO_DAO_Impl.end_spo_thermostat_id_thermostat_event_heat, t_id);
+				
+		//ef11 start db verification
+		WaitUtil.tinyWait();
+		SPO_DAO_Impl.end_SPO_ef11_heat(t_id, setting_phase_0_end);
+		Assert.assertEquals(SPO_DAO_Impl.end_spo_thermostat_id_thermostat_algoritm_heat, t_id);
+		System.out.println("SPO was correctly ended. SPO verification is done.");
+	}
+	
 	@Test //PLAT - 486
 	public void createSpoMODuringSPO(){
 		//verify t_stat is in schedule
